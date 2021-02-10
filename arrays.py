4f0c8cb41d4242743765f407a1d5b3edec380b6c
Fernando Flugel Assad <fernandofassad@gmail.com>
2021-02-10 17:59:25 -0300

    commit

---
+++ arrays.py
@@ -0,0 +1,19 @@
+import numpy as np 
+
+# Just some testing code for now
+
+'''
+value:            [...]
+net change:       [...]
+'''
+
+previous_table = np.array([[10, 10, 10, 10, 10, 10, 10, 10, 10, 10], np.zeros(10)])
+table = np.array([[10, 10, 10, 10, 8, 10, 9, 10, 10, 10], np.zeros(10)])
+
+for i in range(10):
+    table[1, i] = table[0, i] - previous_table[0, i]
+
+print("Past:")
+print(previous_table)
+print("Current:")
+print(table)
