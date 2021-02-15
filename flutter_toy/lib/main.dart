a8d09484eb1c77008a8199e704e66896c94cf84a
Fernando Flugel Assad <fernandofassad@gmail.com>
2021-02-15 16:14:10 -0300

    commit

--- flutter_toy/lib/main.dart
+++ flutter_toy/lib/main.dart
@@ -9,23 +9,44 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Prototype',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Prototype 1'),
-        ),
-        body: Center(
-          child: Prototype(),
-        ),
-      ),
+      home: Prototype(),
     );
   }
 }
 
 class PrototypeState extends State<Prototype> {
+  final List<String> headers = ["Home", "Forum", "Profile", "Exit"]; // Elements
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
   @override
   Widget build(BuildContext context) {
-    final Text content = Text('It is working...');
-    return content;
+    return Scaffold (
+      appBar: AppBar(
+        title: Text('Prototype'), // Changes the name on top
+      body: buildHeaders(),
+    );
+  }
+
+  Widget buildHeaders() {
+    // iterates over all list items
+    return ListView.builder(
+        itemCount: 2 * headers.length, // Specify the length of the list
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+          final int index = i ~/ 2;
+          return buildRow(headers[index]);
+        });
+  }
+
+  Widget buildRow(String element) {
+    return ListTile(
+      title: Text(
+        element,
+        style: _biggerFont,
+      ),
+    );
   }
 }
 
