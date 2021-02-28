60f08e8bd9499c88d8452e78171872069feb5226
Fernando Flugel Assad <fernandofassad@gmail.com>
2021-02-28 08:57:12 -0300

    commit

--- mult_print.py
+++ mult_print.py
@@ -2,7 +2,7 @@ L = range(NUMBER, 0, -1)
 def multiple_prints(_list):
     try:
         dummy = 100 / _list[0]
-        print(':)', _list[0])
+        print(':)' + str(_list[0]))
         multiple_prints(_list[1:])
     except:
         exit
