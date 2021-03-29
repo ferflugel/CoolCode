691988252cff946da3997cb5bee710f0cabc3e6d
Fernando Flugel Assad <fernandofassad@gmail.com>
2021-03-28 22:15:47 -0300

    commit

--- sandbox.py
+++ sandbox.py
@@ -1,19 +1,2 @@
-def f():
-    print('hi')
-def five(k):
-    k()
-    k()
-    k()
-    k()
-    k()
-def four(l):
-    l()
-    l()
-    l()
-    l()
-z = lambda : four(lambda : five(lambda : five(f)))
-w = lambda : four(f)
-def final():
-    z()
-    w()
-final()
+if 1 == 1 and 1 == 1 and 3 == 3:
+    print("hi")
